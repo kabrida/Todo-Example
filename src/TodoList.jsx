2e76969509cb4ecf08728a33dc31b7f062c9021b
Ida Kabris <bhi273@myy.haaga-helia.fi>
2024-09-29 17:18:52 +0300

    All excercises completed
    Todolist sovellus, Todolist poisto toiminnolla, Komponenttien pilkkominen

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,30 +1,46 @@
 import { useState } from "react";
 import TodoTable from "./TodoTable";
-
+//Lähteenä: https://www.w3schools.com/tags/att_input_type_date.asp, https://www.w3schools.com/jsref/jsref_tolocaledatestring.asp, https://www.shecodes.io/athena/1930-passing-data-from-a-child-component-to-parent
 export default function TodoList() {
 
-    const [desc, setDesc] = useState("");
+    const [formData, setFormData] = useState({desc:"", date:""});
     const [todos, setTodos] = useState([]);
 
     const handleChange = (event) => {
-        setDesc(event.target.value);
+        const {name, value} = event.target;
+        setFormData({...formData, [name]: value});
       };
       
+      const formatDate = (dString) => {
+        const d = new Date(dString);
+        return d.toLocaleDateString();
+      }
+
       // Remember to call preventDefault() if using form
       const addTodo = () => {
-        if (!desc) {
-            alert('Please write a description');
+        const {desc, date} = formData;
+        if (!desc || !date) {
+            alert('Please insert description and date');
         } else {
-        setTodos([...todos, desc]);
-        setDesc("");
+        setTodos([...todos, {description: desc, date: formatDate(date)}]);
+        setFormData({desc:"", date: ""});
         }
       };
 
+      const deleteTodo = (index) => {
+        setTodos(todos.filter((todo, i) => i !== index));
+      }
+
     return(
       <>
-      <input placeholder="Description" onChange={handleChange} value={desc} />
-      <button onClick={addTodo}>Add</button>
-      <TodoTable todos={todos} />
+      <h1>My Todolist</h1>
+      <div className="container">
+        <div className="add-title">Add todo</div>
+      <input name="desc" placeholder="Description" onChange={handleChange} value={formData.desc} />
+      <input type="date" name="date" placeholder="Date" onChange={handleChange} value={formData.date} />
+      <button className="add-todo-button" onClick={addTodo}>Add</button>
+      </div>
+      <TodoTable todos={todos} deleteTodo={deleteTodo} />
       </>
     );
   }
--- src/TodoTable.jsx
+++ src/TodoTable.jsx
@@ -2,9 +2,19 @@ function TodoTable(props) {
     return (
         <table>
             <tbody>
+                {props.todos.length > 0 && (
+                <tr>
+                    <th>Date</th>
+                    <th>Description</th>
+                </tr>
+                )}
                 {props.todos.map((item, index) => (
                     <tr key={index}>
-                        <td>{item}</td>
+                        <td>{item.date}</td>
+                        <td>{item.description}</td>
+                        <td>
+                            <button onClick={() => props.deleteTodo(index)}>Done</button>
+                        </td>
                     </tr>
                 ))}
             </tbody>
